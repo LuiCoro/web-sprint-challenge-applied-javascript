53fc43d7ee8666b95ca63a5ee1e58cde60e8cc10
LuiCoro <luiacorona029@icloud.com>
2021-07-18 19:38:16 -0700

    'Card' Completed!

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,93 @@
-const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
-}
+import axios from 'axios';
 
-const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
+const Card = article => {
+	// TASK 5
+	// ---------------------
+	// Implement this function, which should return the markup you see below.
+	// It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
+	// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+	// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+	// Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+	//
+	// <div class="card">
+	//   <div class="headline">{ headline }</div>
+	//   <div class="author">
+	//     <div class="img-container">
+	//       <img src={ authorPhoto }>
+	//     </div>
+	//     <span>By { authorName }</span>
+	//   </div>
+	// </div>
+	//
 
-export { Card, cardAppender }
+	const card = document.createElement('div');
+	const headline = document.createElement('div');
+	const author = document.createElement('div');
+	const container = document.createElement('div');
+	const authorPhoto = document.createElement('img');
+	const authorName = document.createElement('span');
+
+	card.appendChild(headline);
+	card.appendChild(author);
+	author.appendChild(container);
+	container.appendChild(authorPhoto);
+	author.appendChild(authorName);
+
+	card.classList.add('card');
+	headline.classList.add('headline');
+	author.classList.add('author');
+	container.classList.add('img-container');
+
+	headline.textContent = article.headline;
+	authorPhoto.setAttribute('src', article.authorPhoto);
+	authorName.textContent = `By ${article.authorName}`;
+
+	card.addEventListener('click', () => {
+		console.log(article.headline);
+	});
+
+	return card;
+};
+
+const cardAppender = selector => {
+	// TASK 6
+	// ---------------------
+	// Implement this function that takes a css selector as its only argument.
+	// It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
+	// However, the articles do not come organized in a single, neat array. Inspect the response closely!
+	// Create a card from each and every article object in the response, using the Card component.
+	// Append each card to the element in the DOM that matches the selector passed to the function.
+	//
+
+	axios.get('http://localhost:5000/api/articles').then(res => {
+		const data = res.data.articles;
+		const javaScript = data.javascript;
+		const bootStrap = data.bootstrap;
+		const technology = data.technology;
+		const jquery = data.jquery;
+		const node = data.node;
+		const entryPoint = document.querySelector(selector);
+
+		javaScript.forEach(item => {
+			entryPoint.appendChild(Card(item));
+		});
+
+		bootStrap.forEach(item => {
+			entryPoint.appendChild(Card(item));
+		});
+
+		technology.forEach(item => {
+			entryPoint.appendChild(Card(item));
+		});
+
+		jquery.forEach(item => {
+			entryPoint.appendChild(Card(item));
+		});
+
+		node.forEach(item => {
+			entryPoint.appendChild(Card(item));
+		});
+	});
+};
+
+export { Card, cardAppender };
